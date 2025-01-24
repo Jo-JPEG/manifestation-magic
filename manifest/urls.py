57fc8deb601d7e6e1af553f361777cccdc4b4c3f
Jo <joanne.c.wright@googlemail.com>
2025-01-24 17:54:00 +0000

    restrict access for non public manifestations

--- manifest/urls.py
+++ manifest/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.create_manifestation, name='create_manifestation'),
-    path('manifestation/<int:id>/', views.view_manifestation, name='view_manifestation'),
-    path('manifestation/<int:id>/edit/', views.edit_manifestation, name='edit_manifestation'),
-    path('charge/<int:id>/', views.charge_manifestation, name='charge_manifestation'),
+    path('manifestation/<int:manifestation_id>/', views.view_manifestation, name='view_manifestation'),
+    path('manifestation/edit/<int:id>/', views.edit_manifestation, name='edit_manifestation'),
+    path('manifestation/charge/<int:id>/', views.charge_manifestation, name='charge_manifestation'),
     path('manifestation/<int:id>/delete/', views.delete_manifestation, name='delete_manifestation'),
     # ...existing code...
 ]
--- manifest/views.py
+++ manifest/views.py
@@ -4,29 +4,33 @@ from django.utils import timezone
 from datetime import timedelta
 from .models import Manifestation
 from .forms import ManifestationForm  # Assuming you have a form for Manifestation
+from django.http import HttpResponseForbidden
 
 @login_required
 def home(request):
-    user = request.user
-    manifestations = user.manifestations.all()
-    print(f"User: {user.username}, Manifestations: {manifestations}")  # Debug statement
-    return render(request, 'manifest/home.html', {'user': user, 'manifestations': manifestations})
+    manifestations = Manifestation.objects.filter(owner=request.user)
+    return render(request, 'manifest/home.html', {'manifestations': manifestations})
 
 @login_required
 def create_manifestation(request):
     if request.method == 'POST':
         form = ManifestationForm(request.POST)
         if form.is_valid():
-            manifestation = form.save()
-            request.user.manifestations.add(manifestation)
-            return redirect('view_manifestation', id=manifestation.id)
+            manifestation = form.save(commit=False)
+            manifestation.owner = request.user
+            manifestation.save()
+            return redirect('view_manifestation', manifestation_id=manifestation.id)
     else:
         form = ManifestationForm()
     return render(request, 'manifest/create_manifestation.html', {'form': form})
 
 @login_required
-def view_manifestation(request, id):
-    manifestation = Manifestation.objects.get(id=id)
+def view_manifestation(request, manifestation_id):
+    manifestation = get_object_or_404(Manifestation, id=manifestation_id)
+    
+    if manifestation.owner != request.user and not (manifestation.is_public and manifestation.is_approved):
+        return HttpResponseForbidden("You do not have permission to view this manifestation.")
+    
     if manifestation.is_charged and timezone.now() > manifestation.last_charged + timedelta(minutes=2):
         manifestation.is_charged = False
         manifestation.save()
@@ -47,7 +51,7 @@ def charge_manifestation(request, id):
         manifestation.can_charge = False
         manifestation.last_charged = timezone.now()
         manifestation.save()
-        return redirect('view_manifestation', id=id)
+        return redirect('view_manifestation', manifestation_id=id)
     return render(request, 'manifest/view_manifestation.html', {'manifestation': manifestation})
 
 @login_required
@@ -57,7 +61,7 @@ def edit_manifestation(request, id):
         form = ManifestationForm(request.POST, instance=manifestation)
         if form.is_valid():
             form.save()
-            return redirect('view_manifestation', id=manifestation.id)
+            return redirect('view_manifestation', manifestation_id=manifestation.id)
     else:
         form = ManifestationForm(instance=manifestation)
     return render(request, 'manifest/edit_manifestation.html', {'form': form, 'manifestation': manifestation})
@@ -67,5 +71,4 @@ def delete_manifestation(request, id):
     manifestation = get_object_or_404(Manifestation, id=id)
     if request.method == 'POST':
         manifestation.delete()
-        return redirect('home')
-    return render(request, 'manifest/delete_manifestation.html', {'manifestation': manifestation})
+    return redirect('home')
