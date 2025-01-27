61ff45f93baaee2f39e6992a3d074d0dba434b18
Jo <joanne.c.wright@googlemail.com>
2025-01-27 11:05:20 +0000

    update login form

--- account/views.py
+++ account/views.py
@@ -1,19 +1,21 @@
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.display_name = form.cleaned_data.get('display_name')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
 
 def login_view(request):
