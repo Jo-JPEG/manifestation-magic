3f197ac461bd5f8974e999731963a6a70fea2531
Jo <joanne.c.wright@googlemail.com>
2025-01-27 10:40:31 +0000

    fix setting for deployment

--- pixelwitch/settings.py
+++ pixelwitch/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "manifestation-magic-d55b183502af.herokuapp.com"]
 
-CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 'manifestation-magic-d55b183502af.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 'https://*.herokuapp.com']
 
 # Application definition
 
