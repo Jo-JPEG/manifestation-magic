126a03c47b3f3b36433eacffcd970e71d1c211dd
Jo <joanne.c.wright@googlemail.com>
2025-01-28 12:01:07 +0000

    Fix signup form error

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from .forms import SignUpForm
 
 def signup_view(request):
@@ -7,16 +7,18 @@ def signup_view(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.display_name = form.cleaned_data.get('display_name')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            display_name = form.cleaned_data.get('display_name')
+            user.display_name = display_name  # Directly set the display_name
             user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
+            user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('home')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
 
 def login_view(request):
     return render(request, 'registration/login.html')
