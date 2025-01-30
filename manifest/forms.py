cb13c2f651995aab86ec7e866b1519c973c351de
Jo <joanne.c.wright@googlemail.com>
2025-01-30 20:30:39 +0000

    Customise ManifestationForm is_public label and help text

--- manifest/forms.py
+++ manifest/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Manifestation
 
 class ManifestationForm(forms.ModelForm):
+    is_public = forms.BooleanField(required=False, label='Make public?', help_text='Check this box to share your manifestation. Otherwise, it will be private.')
     class Meta:
         model = Manifestation
         fields = ['title', 'description', 'style_choice', 'is_public',]
