1f49d1cdb307f2a478ba7db7ba0d25632cef3601
Jo <joanne.c.wright@googlemail.com>
2025-02-09 19:38:28 +0000

    update readme and testing.md

--- manifest/admin.py
+++ manifest/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Manifestation
 
+
 class PublicNotApprovedFilter(admin.SimpleListFilter):
     title = 'Public but not approved'
     parameter_name = 'public_not_approved'
@@ -15,14 +16,20 @@ class PublicNotApprovedFilter(admin.SimpleListFilter):
             return queryset.filter(is_public=True, is_approved=False)
         return queryset
 
+
 def approve_manifestations(modeladmin, request, queryset):
     queryset.update(is_approved=True)
-approve_manifestations.short_description = "Approve selected manifestations"
+
+
+approve_manifestations.short_description = "Approve selected"
+
 
 class ManifestationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'owner', 'is_public', 'is_approved')
+    list_display = (
+        'title', 'description', 'owner', 'is_public', 'is_approved')
     list_filter = ('is_public', 'is_approved', PublicNotApprovedFilter)
     actions = [approve_manifestations]
     prepopulated_fields = {'slug': ('title',)}
+
 
 admin.site.register(Manifestation, ManifestationAdmin)
